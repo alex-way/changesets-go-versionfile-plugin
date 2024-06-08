3869c051ee20b9d072a9b96f722f42bd01691697
Alex Way <way.alexander@yahoo.co.uk>
2024-06-08 09:55:35 +0100

    refactor: Move proto file to src/protos

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 extern crate prost_build;
 
 fn main() {
-    prost_build::compile_protos(&["src/plugin.proto"], &["src/"]).unwrap();
+    prost_build::compile_protos(&["src/protos/plugin.proto"], &["src/protos/"]).unwrap();
 }
